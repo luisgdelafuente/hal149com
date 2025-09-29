612a7908e28027dd4c0190b1963a20c081db615c
HAL149 <luisgdelafuente@gmail.com>
2025-09-29 13:44:00 +0200

    .

---
+++ src/pages/es/posts/index.json.js
@@ -0,0 +1,25 @@
+export async function GET() {
+  const { getCollection } = await import('astro:content');
+  const all = await getCollection('posts');
+  const posts = all
+    .filter(p => p.data.lang === 'es')
+    .sort((a, b) => new Date(b.data.date).getTime() - new Date(a.data.date).getTime())
+    .map(p => ({
+      slug: p.slug,
+      title: p.data.title,
+      description: p.data.description,
+      author: p.data.author,
+      tags: p.data.tags || [],
+      date: p.data.date instanceof Date ? p.data.date.toISOString() : p.data.date,
+      image: p.data.image || null
+    }));
+
+  return new Response(JSON.stringify({ posts }), {
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+      'Cache-Control': 'public, max-age=0, s-maxage=31536000, immutable'
+    }
+  });
+}
+
+
